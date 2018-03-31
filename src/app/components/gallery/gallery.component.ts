9833d8580a537921fdf639cc8dbceaff20a51b7b
Jérôme WILMART <jwilmart@maltem.com>
2018-03-31 20:21:22 +0200

    grid

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -22,7 +22,7 @@ export class GalleryComponent implements OnInit {
      this.node = document.createElement('img');
      this.node.setAttribute('src', '../../../assets/Photo 1 (' + this.img.toString() + ').jpg');
      this.node.setAttribute('width', '20%');
-     this.node.setAttribute('class', 'col-3');
+     this.node.setAttribute('class', 'col-md-3 col-12');
      this.node.setAttribute('style', 'margin-bottom:2%')
      document.getElementById('gallery').appendChild(this.node);
      console.log(this.node);
